25c1b36c6a1ec24143a4664398eb4c0616b12b77
Rohan Sinha <sinharohan3@gmail.com>
2023-01-03 15:46:21 -0500

    Update Final_Code.py

--- Restaurant Recommendation/Final_Code.py
+++ Restaurant Recommendation/Final_Code.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
+
 # Reading up the data
 pd.set_option('display.max_columns', 100)
 df = pd.read_csv('zomato.csv',nrows = 5000)
